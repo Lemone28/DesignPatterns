470bdd803087a5ee27a57d4e4194925549193945
Lemone28 <lemone681@gmail.com>
2023-05-30 18:11:49 +0300

    Implementations of pattern Singleton in FileReader class

--- src/main/java/com/patterns/singlton/FileReader.java
+++ src/main/java/com/patterns/singlton/FileReader.java
@@ -1,5 +1,16 @@
 package com.patterns.singlton;
 
 public class FileReader {
+    private static FileReader instance;
 
+    private FileReader() {
+
+    }
+
+    public static FileReader getInstance() {
+        if (instance == null) {
+            instance = new FileReader();
+        }
+        return instance;
+    }
 }
