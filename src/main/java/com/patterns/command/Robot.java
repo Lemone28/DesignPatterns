146e43e3c35bbb7d3ee50bc43be77197d5791d3b
Lemone28 <lemone681@gmail.com>
2023-05-31 00:30:54 +0300

    Create Robot class

---
+++ src/main/java/com/patterns/command/Robot.java
@@ -0,0 +1,10 @@
+package com.patterns.command;
+
+public class Robot {
+    public void moveForward() {
+        System.out.println("Робот рухається в перед");
+    }
+    public void moveBackward() {
+        System.out.println("Робот рухається назад");
+    }
+}
