619deff519f6a015c3abc33ce59cbb03dd8f51a1
Lemone28 <lemone681@gmail.com>
2023-05-31 11:47:39 +0300

    Update main method

--- src/main/java/com/patterns/Main.java
+++ src/main/java/com/patterns/Main.java
@@ -1,7 +1,22 @@
 package com.patterns;
 
+
+import com.patterns.abstractFactory.AbstractFactoryRunnable;
+import com.patterns.command.CommandRunnable;
+import com.patterns.decorator.DecoratorRunnable;
+import com.patterns.proxy.ProxyRunnable;
+import com.patterns.singlton.SingltonRunnable;
+import com.patterns.snapshot.SnapshotRunnable;
+
 public class Main {
     public static void main(String[] args) {
-
+        Runnable.go(
+                AbstractFactoryRunnable.class,
+                SingltonRunnable.class,
+                DecoratorRunnable.class,
+                ProxyRunnable.class,
+                SnapshotRunnable.class,
+                CommandRunnable.class
+        );
     }
 }
